29075eec461570111c107975032a63fefe1ad6a0
Lee Hassall <leehassall@192.168.1.161>
2024-07-26 13:24:29 +0100

    initial app cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'black': '#010101',
+      'light-grey': '#e4e4e4',
+      'dark-purple': '#4b1d3f',
+      'red': '#d62246',
+      'light-green': '#d4f4dd',
+      'cyan': '#17bebb',
+      'dark-cyan': '#0e7c7b',
+    },
   },
   plugins: [],
 }
